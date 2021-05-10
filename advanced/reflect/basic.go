bed2ed81ae3a0f01126d95b001ebc8113b37ee58
mostafa <radionishi@gmail.com>
2021-05-11 00:19:17 +0600

    interface

--- advanced/reflect/basic.go
+++ advanced/reflect/basic.go
@@ -6,6 +6,7 @@ import (
 )
 
 // https://golang.org/pkg/reflect/
+// https://betterprogramming.pub/understand-reflect-in-go-24a68fcf1011
 
 func main() {
 	var name string = "kamal"
--- hello.go
+++ hello.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
 	var name string = "Golang" + " " + "Lovers"
 	fmt.Println("Hello, ", name)
 
